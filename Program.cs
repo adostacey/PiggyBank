91bf3ca3fd6659f0bcba8b71e1fec5f4e6e45af1
Aaron <140290205+adostacey@users.noreply.github.com>
2025-07-18 13:21:23 -0500

    intial testing of class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PiggyBank;
+
+Account account1 = new() { Name = "Aaron", AccountType = AccountTypes.Checking };
+
+Console.WriteLine($"Name: {account1.Name}");
+Console.WriteLine($"Account Type: {account1.AccountType}");
+Console.WriteLine($"Balance: {account1.Balance}");
+
+account1.Deposit(300);
+Console.WriteLine($"Balance: {account1.Balance}");
+
+account1.WithDraw(50);
+Console.WriteLine($"Balance: {account1.Balance}");
