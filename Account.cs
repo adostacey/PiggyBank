181d66aa056c0f723461175a8db5d8b177237007
Aaron <140290205+adostacey@users.noreply.github.com>
2025-07-18 13:59:05 -0500

    add: error handling

--- Account.cs
+++ Account.cs
@@ -2,15 +2,31 @@
 
 public class Account
 {
-
     public required string Name { get; set; }
     public decimal Balance { get; private set; }
     public required AccountTypes AccountType { get; init; }
 
     public void Deposit(decimal amount)
-    { Balance += amount; }
+    {
+        ValidAmmount(amount);
+        Balance += amount;
+    }
 
     public void WithDraw(decimal amount)
-    { Balance -= amount; }
+    {
+        ValidAmmount(amount);
+        if (amount < Balance)
+        {
+            Balance -= amount;
+        }
 
+        else
+        { throw new ArgumentException("Not Enough Funds In Account"); }
+    }
+
+    private static void ValidAmmount(decimal amount)
+    {
+        if (amount < 0)
+        { throw new ArgumentException("Amount must not be negative"); }
+    }
 }
