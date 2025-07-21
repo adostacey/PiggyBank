8360320073ae7b8b05336d7b5c164e7a281270d3
Aaron <140290205+adostacey@users.noreply.github.com>
2025-07-21 15:31:08 -0500

    feat: add menu

--- Program.cs
+++ Program.cs
@@ -1,33 +1,11 @@
 ﻿using PiggyBank;
+using PiggyBank.ui.Menu;
+using PiggyBank.ui.MenuItems;
 
 Account account1 = new() { Name = "Aaron", AccountType = AccountTypes.Checking };
 
-Console.WriteLine($"Name: {account1.Name}");
-Console.WriteLine($"Account Type: {account1.AccountType}");
-Console.WriteLine($"Balance: {account1.Balance}");
-
-account1.Deposit(300);
-Console.WriteLine($"Balance: {account1.Balance}");
-
-account1.WithDraw(50);
-Console.WriteLine($"Balance: {account1.Balance}");
-
-/*
-try
-{
-    account1.WithDraw(500);
-}
-catch (ArgumentException ex)
-{
-    Console.WriteLine(ex.Message);
-}
-*/
-
-try
-{
-    account1.WithDraw(-1);
-}
-catch (ArgumentException ex)
-{
-    Console.WriteLine(ex.Message);
-}
+Menu menu = new();
+menu.AddMenuItem(new CheckBalance());
+menu.AddMenuItem(new DepositFunds());
+menu.AddMenuItem(new WithdrawFunds());
+menu.Show();
